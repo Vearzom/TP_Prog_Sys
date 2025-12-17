564906bb4cb0ca99b9a5f0c0a90e9f34ba1debe0
[Vearzom] <[elise.castan@laposte.net]>
2025-12-17 15:50:17 +0100

    Macro corrections in Q4

--- Q4.c
+++ Q4.c
@@ -12,11 +12,13 @@
 #define PERMANENT "enseash "
 #define SIZE_PERMANENT 9
 #define BUF_SIZE 128
+#define EXIT "exit"
+#define SIGNAL "signal"
 
 void print_status(int status){
     char buf[BUF_SIZE];
     
-    int temp=snprintf(buf, BUF_SIZE, "%s[%s:%d]%%", PERMANENT, WIFEXITED(status)?"exit":"signal",
+    int temp=snprintf(buf, BUF_SIZE, "%s[%s:%d]%%", PERMANENT, WIFEXITED(status)?EXIT:SIGNAL,
              WIFEXITED(status)?WEXITSTATUS(status):WTERMSIG(status));
     if(write(STDOUT_FILENO, buf, temp) == -1){
         perror("Write");
