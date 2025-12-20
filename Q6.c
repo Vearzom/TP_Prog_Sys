a399d4cee1ac1f7215f72f15bc7d968d24604ddd
[Vearzom] <[elise.castan@laposte.net]>
2025-12-20 18:32:54 +0100

    Add parsing in question 6

--- Q6.c
+++ Q6.c
@@ -7,15 +7,41 @@
 #include "Q6.h"
 #include <time.h>
 
-char ** execute_complex_cmd(int * argc, char* command){
-    char ** args[16];
-    char * flag = strtok(command, SEPARATOR);
+char ** parsing_cmd(int * argc, char* command){
+    static char *args[MAX_ARGS];
+    char *flag = strtok(command, SEPARATOR);
     int index = 0;
-    while (flag != NULL) {
+    while (flag != NULL && index < MAX_ARGS - 1) {
         args[index++] = flag;
         flag = strtok(NULL, SEPARATOR);
     }
     args[index] = NULL;
     *argc = index;
+
     return args;
 } 
+
+void execute_cmd(char *command) {
+    int argc;
+    char **args = parsing_cmd(&argc, command);
+
+    if (argc == 0) return;  // nothing to execute
+
+    pid_t pid = fork();  // create a child process
+    if (pid < 0) {
+        perror("fork failed");
+        return;
+    }
+
+    if (pid == 0) {
+        // Child process
+        if (execvp(args[0], args) < 0) {
+            perror("execvp failed");
+            exit(1);
+        }
+    } else {
+        // Parent process waits for child to finish
+        int status;
+        waitpid(pid, &status, 0);
+    }
+}
--- Q6.h
+++ Q6.h
@@ -2,8 +2,9 @@
 #define Q6_H
 
 #define SEPARATOR " "
-#define MAX_ARGS 32
+#define MAX_ARGS 16
 
-void decoupe();
+char ** passing_cmd(int * argc, char* command);
+int execute_cmd_complex(int *argc,char* cmd);
 
 #endif
